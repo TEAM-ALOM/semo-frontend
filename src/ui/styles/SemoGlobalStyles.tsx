51ee499bde1dac4176ef5d836ed765edd4e5cee0
ghoon99 <ghoon99@kakao.com>
2024-10-31 14:01:32 +0900

    feat: 폰트 설정

--- src/ui/styles/SemoGlobalStyles.tsx
+++ src/ui/styles/SemoGlobalStyles.tsx
@@ -6,8 +6,8 @@ export const SemoGlobalStyles = createGlobalStyle`
   ${miniReset}
 
   body {
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Apple SD Gothic Neo", "Pretendard Variable", Pretendard, Roboto, "Noto Sans KR", "Segoe UI", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     background-color: black;
-    color:white
+    color: white;
   }
 `;
