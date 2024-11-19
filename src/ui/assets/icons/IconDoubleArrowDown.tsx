e38f594d6f454460c65181f238dfe602cc87b78b
ghoon99 <ghoon99@kakao.com>
2024-11-19 15:53:28 +0900

    feat:  double arrow down icon 추가

---
+++ src/ui/assets/icons/IconDoubleArrowDown.tsx
@@ -0,0 +1,15 @@
+// icon:double-arrow-down | Radix Icons https://icons.radix-ui.com/ | WorkOS
+import { SVGProps } from 'react';
+
+export const IconDoubleArrowDown = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg fill='none' viewBox='0 0 15 15' height='1em' width='1em' {...props}>
+      <path
+        fill='currentColor'
+        fillRule='evenodd'
+        d='M3.854 2.146a.5.5 0 10-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 00-.708-.708L7.5 5.793 3.854 2.146zm0 6a.5.5 0 10-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 00-.708-.708L7.5 11.793 3.854 8.146z'
+        clipRule='evenodd'
+      />
+    </svg>
+  );
+};
