bc117141fcba78fbae1c56743e01060c93236f46
ghoon99 <ghoon99@kakao.com>
2024-11-20 16:31:38 +0900

    chore: 아이콘 add more 추가

---
+++ src/ui/assets/icons/IconMoreVertical.tsx
@@ -0,0 +1,12 @@
+import { SVGProps } from 'react';
+
+export const IconMoreVertical = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg fill='none' viewBox='0 0 15 15' height='1em' width='1em' {...props}>
+      <path
+        stroke='currentColor'
+        d='M7.5 3a.5.5 0 110-1 .5.5 0 010 1zM7.5 8a.5.5 0 110-1 .5.5 0 010 1zM7.5 13a.5.5 0 110-1 .5.5 0 010 1z'
+      />
+    </svg>
+  );
+};
